08fa584f3e863c1021c14b886f507e05b7082fff
minpyaesone <77276239+minpyaesone82@users.noreply.github.com>
2022-02-22 10:04:26 +0630

    Add files via upload

---
+++ project/js/app.js
@@ -0,0 +1,24 @@
+var animateButton = function(e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  var bubblyButtons = document.getElementsByClassName("bubbly-button");
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+  } 
+
+
+  $(window).on("load",function(){
+    $(".loader-container").fadeOut(500,function(){
+      $(".loader-container").remove();
+    });
+  });
